4db5baf73d6c083d52cbf8ee639f3cf4d80083c6
Joe VanLeeuwen <jvnlwn@gmail.com>
2013-11-12 21:01:29 -0500

    clarified some things

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,70 @@
 var count = 0;
 var timeOut;
 
-
 var projectList = [
 
 	{
-		info:  'A 3-week project implementing the rules of chess.',
-		link:  'http://joe-vanleeuwen.github.io/chess.js',
-		image: '../images/chess.png'
+		info:      'A 3-week project implementing the rules of chess.',
+		siteLink:  'http://joe-vanleeuwen.github.io/chess.js',
+		gitLink:   'https://github.com/joe-vanleeuwen/chess.js',
+		image:     '../images/chess.png'
 	},
 
 	{
-		info:  'This just looks nice.',
-		link:  'http://joe-vanleeuwen.github.io/checkerboard',
-		image: '../images/some-squares.png'
+		info:      'This just looks nice.',
+		siteLink:  'http://joe-vanleeuwen.github.io/checkerboard',
+		gitLink:   'https://github.com/joe-vanleeuwen/checkerboard',
+		image:     '../images/some-squares.png'
 	},
 	{
-		info:  'This is a simple to-do list using Parse.',
-		link:  'http://joe-vanleeuwen.github.io/to-do-list',
-		image: '../images/to-do.png'
+		info:      'This is a simple to-do list using Parse.',
+		siteLink:  'http://joe-vanleeuwen.github.io/to-do-list',
+		gitLink:   'https://github.com/joe-vanleeuwen/to-do-list',
+		image:     '../images/to-do.png'
 	},
 	{
-		info:  'My first markup practice.',
-		link:  'http://joe-vanleeuwen.github.io/mobber',
-		image: '../images/mobber.png'
+		info:      'My first markup practice.',
+		siteLink:  'http://joe-vanleeuwen.github.io/mobber',
+		gitLink:   'https://github.com/joe-vanleeuwen/mobber',
+		image:     '../images/mobber.png'
 	},	
 	{
-		info:  'A group project chat program.',
-		link:  'http://joe-vanleeuwen.github.io/buzzME',
-		image: '../images/buzz-me.png'
+		info:      'A group project chat program.',
+		siteLink:  'http://joe-vanleeuwen.github.io/buzzME',
+		gitLink:   'https://github.com/joe-vanleeuwen/buzzME',
+		image:     '../images/buzz-me.png'
 	}
 ]
+
+// var projectList = [
+
+// 	{
+// 		info:  'A 3-week project implementing the rules of chess.',
+// 		link:  'http://joe-vanleeuwen.github.io/chess.js',
+// 		image: '../images/chess.png'
+// 	},
+
+// 	{
+// 		info:  'This just looks nice.',
+// 		link:  'http://joe-vanleeuwen.github.io/checkerboard',
+// 		image: '../images/some-squares.png'
+// 	},
+// 	{
+// 		info:  'This is a simple to-do list using Parse.',
+// 		link:  'http://joe-vanleeuwen.github.io/to-do-list',
+// 		image: '../images/to-do.png'
+// 	},
+// 	{
+// 		info:  'My first markup practice.',
+// 		link:  'http://joe-vanleeuwen.github.io/mobber',
+// 		image: '../images/mobber.png'
+// 	},	
+// 	{
+// 		info:  'A group project chat program.',
+// 		link:  'http://joe-vanleeuwen.github.io/buzzME',
+// 		image: '../images/buzz-me.png'
+// 	}
+// ]
 
 // first and last are the same for sliding purposes
 projectList.push(projectList[0])
